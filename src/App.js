d93d4c8b2dfafcb0198dbfc6e520b6c4cdf3d14b
kbpsystem777 <koleen.bp@outlook.com>
2021-04-18 02:50:47 +0800

    modifying app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import Header from "./components/Header"
 import Home from "./components/pages/Home"
 import Login from "./components/pages/Login"
+import OwnerRegistration from "./components/pages/registration/OwnerRegistration"
+import TenantRegistration from "./components/pages/registration/TenantRegistration"
 import Footer from "./components/Footer"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./styles/app.css"
 
 function App() {
   return (
-    <div className="app">
-      <Home className="text-center" />
-      <Login />
+    <Router className="app container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register/owner" exact component={OwnerRegistration} />
+        <Route path="/register/tenant" exact component={TenantRegistration} />
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,16 @@
-import React from "react"
+const contents = require("../../data/contents.json")
 
 function Home() {
   return (
-    <div className="jumbotron">
-      <div>
-        <h1>HomeRenter</h1>
+    <div className="container app-container">
+      <div className="container">
+        <h5 className="text-center">Home Page</h5>
+        <div>
+          <p>{contents.homepage}</p>
+        </div>
+        <div>
+          <p>{contents.homepage}</p>
+        </div>
       </div>
     </div>
   )
